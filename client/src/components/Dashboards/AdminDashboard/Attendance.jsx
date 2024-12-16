6ba9ca0a1411d22449a6e4cf4202f4a63d920389
DevyanshuNegi <devyanshunegi@gmail.com>
2024-12-17 01:50:33 +0530

    bug fixes

--- client/src/components/Dashboards/AdminDashboard/Attendance.jsx
+++ client/src/components/Dashboards/AdminDashboard/Attendance.jsx
@@ -1,261 +1,152 @@
 import { useEffect, useState } from "react";
-import { Doughnut } from "react-chartjs-2";
-import { getAllStudents } from "../../../utils";
+import { Modal } from "./Modal";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import LoadingBar from 'react-top-loading-bar'
+import { Loader } from "../../Dashboards/Common/Loader";
+import axios from "axios";
 
-function Attendance() {
-  const getALL = async () => {
-    setProgress(30);
-    const marked = await fetch("http://localhost:3000/api/attendance/getHostelAttendance", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ hostel: JSON.parse(localStorage.getItem("hostel"))._id }),
-        });
-    setProgress(40);
-    const markedData = await marked.json();
-    setProgress(50)
-    if (markedData.success) {
-      // console.log("Attendance: ", markedData.attendance);
-    }
-    const markedStudents = markedData.attendance.map((student) => {
-      return {
-        id: student.student._id,
-        cms: student.student.cms_id,
-        name: student.student.name,
-        room: student.student.room_no,
-        attendance: student.status === "present" ? true : false,
-      };
-    });
-    setProgress(70);
-    setMarkedStudents(markedStudents);
-    const data = await getAllStudents();
-    const students = data.students;
-    const unmarkedStudents = students.filter(
-      (student) =>
-        !markedStudents.find((markedStudent) => markedStudent.id === student._id)
-    );
-    setProgress(90);
-    unmarkedStudents.map((student) => {
-      student.id = student._id;
-      student.cms = student.cms_id;
-      student.name = student.name;
-      student.room = student.room_no;
-      student.attendance = undefined;
-    });
-    setunmarkedStudents(unmarkedStudents);
-    setProgress(100);
-  };
-
-  const [progress, setProgress] = useState(0)
-  const [unmarkedStudents, setunmarkedStudents] = useState([]);
-  const [markedStudents, setMarkedStudents] = useState([]);
-
-  const markAttendance = async (id, isPresent) => {
-    const data = await fetch(`http://localhost:3000/api/attendance/mark`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ student:id, status: isPresent? "present" : "absent" }),
-    });
-    const response = await data.json();
-    if (response.success) {
-      toast.success(
-        "Attendance Marked Successfully!", {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-      });
-    }
-
-    unmarkedStudents.find((student) => student.id === id).attendance =
-      isPresent;
-    setunmarkedStudents(
-      unmarkedStudents.filter((student) => student.attendance === undefined)
-    );
-    setMarkedStudents((markedStudents) =>
-      markedStudents.concat(
-        unmarkedStudents.filter((student) => student.attendance !== undefined)
-      )
-    );
-  };
-
-  const [present, setPresent] = useState(0);
-
+const Attendance = () => {
+  // const [loading, setLoading] = useState(false);
+  // const [events, setEvents] = useState([{}]);
+  // useEffect(() => {
+  //   return () => {
+  //     axios
+  //       .get("http://localhost:8000/api/v1/events/getUpcomingEvents")
+  //       .then((response) => {
+  //         setEvents(response.data.data);
+  //       });
+  //   };
+  // }, []);
+  const [events, setEvents] = useState([]);
   useEffect(() => {
-    getALL();
-    setPresent(
-      markedStudents.filter((student) => student.attendance === true).length
-    );
-  }, [unmarkedStudents.length, markedStudents.length]);
-
-  let date = new Date();
-  date = date.toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
-
-  const labels = ["Present", "Absentees", "Unmarked Students"];
-  const graph = (
-    <div className="flex flex-row-reverse items-center gap-3 h-64">
-      <Doughnut
-        datasetIdKey="id"
-        data={{
-          labels,
-          datasets: [
-            {
-              label: "No. of Students",
-              data: [
-                present,
-                markedStudents.length - present,
-                unmarkedStudents.length,
-              ],
-              backgroundColor: ["#1D4ED8", "#F26916", "#808080"],
-              barThickness: 20,
-              borderRadius: 0,
-              borderJoinStyle: "round",
-              borderColor: "rgba(0,0,0,0)",
-              hoverOffset: 10,
-            },
-          ],
-        }}
-        options={{
-          plugins: {
-            legend: {
-              display: false,
-            },
-          },
-        }}
-      />
-      <ul className="text-white">
-        <li className="flex gap-2">
-          {" "}
-          <span className="w-10 h-5 bg-orange-500 block"></span> Absent
-        </li>
-        <li className="flex gap-2">
-          {" "}
-          <span className="w-10 h-5 bg-blue-500 block"></span> Present
-        </li>
-      </ul>
+    return () => {
+      const axiosInstance = axios.create({
+        baseURL: "http://localhost:8000/api/v1/",
+        withCredentials: true,
+      });
+      // let name;
+      axiosInstance
+        .get("http://localhost:8000/api/v1/organisation/getAll-events")
+        .then((response) => {
+          setEvents(response.data.data);
+        });
+    };
+  }, []);
+  return (
+    <div className="w-full  p-6 rounded-xl shadow-lg bg-gradient-to-br from-gray-800 to-gray-900 drop-shadow-2xl overflow-y-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {events.map((event, index) => (
+          <div
+            key={index}
+            className="p-4 rounded-lg bg-gray-700 shadow-md hover:shadow-xl transition-all duration-200"
+          >
+            <div className="flex flex-col items-start">
+              <div className="text-white mt-2">
+                <p className="text-xl text-red-300 font-bold">{event.name}</p>
+                <br />
+                <p className="text-xl font-semibold">{event.description}</p>
+                <p className="text-sm text-gray-400">{event.eventDate}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-
-
+};
+function Suggestions() {
+  // const [events, setEvents] = useState([]);
+  // useEffect(() => {
+  //   return () => {
+  //     axios
+  //       .get("http://localhost:8000/api/v1/organisation/getAll-events")
+  //       .then((response) => {
+  //         setEvents(response.data.data);
+  //       });
+  //   };
+  // }, []);
   return (
-    <div className="w-full h-screen flex flex-col gap-3 items-center xl:pt-0 md:pt-40 pt-64 justify-center overflow-auto max-h-screen">
-      <LoadingBar color="#0000FF" progress={progress} onLoaderFinished={() => setProgress(0)} />
-      <h1 className="text-white font-bold text-5xl">Attendance</h1>
-      <p className="text-white text-xl mb-10">Date: {date}</p>
-      <div className="flex gap-5 flex-wrap items-center justify-center">
-        <>{graph}</>
-        <div className="flow-root md:w-[400px] w-full bg-neutral-950 px-7 py-5 rounded-lg shadow-xl max-h-[250px] overflow-auto">
-          <span
-            className={`font-bold text-xl text-white ${
-              unmarkedStudents.length ? "block" : "hidden"
-            }`}
-          >
-            Unmarked Students
-          </span>
-          <ul role="list" className="divide-y divide-gray-700 text-white">
-            {unmarkedStudents.length === 0
-              ? "All students are marked!"
-              : unmarkedStudents.map((student) =>
-                  student.attendance === undefined ? (
-                    <li
-                      className="py-3 sm:py-4 px-5 rounded hover:bg-neutral-700 hover:scale-105 transition-all"
-                      key={student.id}
-                    >
-                      <div className="flex items-center space-x-4">
-                        <div className="flex-shrink-0 text-white">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                            />
-                          </svg>
-                        </div>
-                        <div className="flex-1 min-w-0">
-                          <p className="text-sm font-medium truncate text-white">
-                            {student.name}
-                          </p>
-                          <p className="text-sm truncate text-gray-400">
-                            {student.cms} | Room: {student.room}
-                          </p>
-                        </div>
-                        <button
-                          className="hover:underline hover:text-green-600 hover:scale-125 transition-all"
-                          onClick={() => markAttendance(student.id, true)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                          </svg>
-                        </button>
-                        <button
-                          className="hover:underline hover:text-red-600 hover:scale-125 transition-all"
-                          onClick={() => markAttendance(student.id, false)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </li>
-                  ) : (
-                    ""
-                  )
+    <div className="w-full h-screen flex flex-col gap-3 items-center justify-center">
+      <h1 className="text-white font-bold text-5xl">All Events</h1>
+      {/* <div className="bg-neutral-950 px-10 py-5 rounded-xl shadow-xl sm:w-[50%] sm:min-w-[450px] w-full mt-5 max-h-96 overflow-auto">
+        <span className="text-white font-bold text-xl">All Students</span>
+        <ul role="list" className="divide-y divide-gray-700 text-white">
+          {suggestions.length === 0
+            ? "No Students Suggestion Found"
+            : suggestions.map((suggestion) => (
+              <>
+                {showModal && (
+                  <Modal closeModal={toggleModal} suggestion={modalData} />
                 )}
-          </ul>
-        </div>
+                <li
+                  className="py-3 px-5 rounded sm:py-4 hover:bg-neutral-700 hover:shadow-xl hover:scale-105  transition-all cursor-pointer"
+                  key={suggestion._id}
+                >
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-shrink-0 text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 hover:scale-125 hover:text-yellow-500 transition-all"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+                        />
+                      </svg>
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium truncate text-white">
+                        {suggestion.title}
+                      </p>
+                      <p className="text-sm truncate text-gray-400">
+                        {suggestion.description}
+                      </p>
+                      <button
+                        className="text-blue-500 text-sm underline"
+                        onClick={() => toggleModal(suggestion)}
+                      >
+                        Read more
+                      </button>
+                    </div>
+                    <button className="group/show relative z-0"
+                      onClick={() => updateSuggestion(suggestion._id)}
+                    >
+                      {
+                        loader ? 
+                          <Loader />:
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6 hover:text-green-600 hover:scale-125 transition-all"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+                            />
+                          </svg>
+                      }
+                      <span className="text-sm hidden absolute px-2 -right-10 top-6 bg-black text-center group-hover/show:block rounded">
+                        Acknowledge.
+                      </span>
+                    </button>
+                  </div>
+                </li>
+              </>
+            ))}
+        </ul>
+      </div> */}
+      <div className="w-full mt-8">
+        <AllEvents />
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme= "dark"
-      />
     </div>
   );
 }
